ce6edc1f820a913205aed64687ba34fa40f109f7
Tbrahn <tbranham9323@gmail.com>
2019-03-18 23:54:38 -0400

    add first eventlistener

---
+++ app.js
@@ -0,0 +1,7 @@
+var topLeft = document.getElementById("topleftx");
+
+topLeft.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("topLeftH1").innerHTML = "x";
+}
