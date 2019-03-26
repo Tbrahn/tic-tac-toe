e43b88239137bfb87b1cffa8dd98fda7138c34c1
Tbrahn <tbranham9323@gmail.com>
2019-03-25 23:21:09 -0400

    add js middle row ids

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ topLeftX.addEventListener("click", topLeftFunc);
 topMiddleX.addEventListener("click", topMiddleFunc);
 topRightX.addEventListener("click", topRightFunc);
 
+
 function topLeftFunc() {
     document.getElementById("topleftH1").innerHTML = "x";
 }
@@ -17,4 +18,16 @@ function topMiddleFunc() {
 
 function topRightFunc() {
     document.getElementById("toprightH1").innerHTML = "x";
+}
+
+function middleLeftFunc() {
+    document.getElementById("middleleftH1").innerHTML = "x";
+}
+
+function MiddleFunc() {
+    document.getElementById("middleH1").innerHTML = "x";
+}
+
+function middleRightFunc() {
+    document.getElementById("middlerightH1").innerHTML = "x";
 }
