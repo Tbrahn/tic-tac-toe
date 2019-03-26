67e9a38c5d4bdbc437a7c8a10677637638cb47e0
Tbrahn <tbranham9323@gmail.com>
2019-03-25 22:55:39 -0400

    add top row event listeners

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var topLeft = document.getElementById("topleftx");
+var topLeftX = document.getElementById("topleftx");
+var topMiddleX = document.getElementById("topmiddlex");
+var topRightX = document.getElementById("toprightx");
 
-topLeft.addEventListener("click", myFunction);
 
-function myFunction() {
-    document.getElementById("topLeftH1").innerHTML = "x";
+topLeftX.addEventListener("click", topLeftFunc);
+topMiddleX.addEventListener("click", topMiddleFunc);
+topRightX.addEventListener("click", topRightFunc);
+
+
+
+
+function topLeftFunc() {
+    document.getElementById("topleftH1").innerHTML = "x";
+}
+
+function topMiddleFunc() {
+    document.getElementById("topmiddleH1").innerHTML = "x";
+}
+
+function topRightFunc() {
+    document.getElementById("toprightH1").innerHTML = "x";
 }
