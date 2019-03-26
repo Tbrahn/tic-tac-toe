f18e797090f2044f45df9e3a37b5f228b43de366
Tbrahn <tbranham9323@gmail.com>
2019-03-25 23:25:38 -0400

    add middle row event listeners

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var topLeftX = document.getElementById("topleftx");
 var topMiddleX = document.getElementById("topmiddlex");
 var topRightX = document.getElementById("toprightx");
+var middleLeftX = document.getElementById("middleleftx");
+var MiddleX = document.getElementById("middlex");
+var middleRightX = document.getElementById("middlerightx");
 
 
 topLeftX.addEventListener("click", topLeftFunc);
 topMiddleX.addEventListener("click", topMiddleFunc);
 topRightX.addEventListener("click", topRightFunc);
-
+middleLeftX.addEventListener("click", middleLeftFunc);
+MiddleX.addEventListener("click", MiddleFunc);
+middleRightX.addEventListener("click", middleRightFunc);
 
 function topLeftFunc() {
     document.getElementById("topleftH1").innerHTML = "x";
