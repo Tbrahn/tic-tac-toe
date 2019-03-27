0c90468290b19d4bd499873bdf33fe0ba8c59708
Tbrahn <tbranham9323@gmail.com>
2019-03-27 01:18:04 -0400

    add top row win check function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// button vars
 var topLeftX = document.getElementById("topleftx");
 var topMiddleX = document.getElementById("topmiddlex");
 var topRightX = document.getElementById("toprightx");
@@ -7,11 +8,18 @@ var middleRightX = document.getElementById("middlerightx");
 var bottomLeftX = document.getElementById("bottomleftx");
 var bottomMiddleX = document.getElementById("bottommiddlex");
 var bottomRightX = document.getElementById("bottomrightx");
+// H1 vars
+var topLeftH1 = document.getElementById("topleftH1");
+var topMiddleH1 = document.getElementById("topmiddleH1");
+var topRightH1 = document.getElementById("toprightH1");
 
 
 topLeftX.addEventListener("click", topLeftFunc);
+topLeftX.addEventListener("click", test);
 topMiddleX.addEventListener("click", topMiddleFunc);
+topMiddleX.addEventListener("click", test);
 topRightX.addEventListener("click", topRightFunc);
+topRightX.addEventListener("click", test);
 middleLeftX.addEventListener("click", middleLeftFunc);
 MiddleX.addEventListener("click", MiddleFunc);
 middleRightX.addEventListener("click", middleRightFunc);
@@ -53,4 +61,14 @@ function bottomMiddleFunc() {
 
 function bottomRightFunc() {
     document.getElementById("bottomrightH1").innerHTML = "x";
+}
+
+function test() {
+    if(topLeftH1.innerText === topMiddleH1.innerText && topMiddleH1.innerText  === topRightH1.innerText){
+        alert("you win!");
+        console.log("You win!")
+    } else {
+        console.log("you dont win yet!")
+    }
+    console.log("test");
 }
